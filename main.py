d59b7fa0634b300588d4eda5a5f3acfdc066b74d
guanyu.yi <guanyu.yi@invo.cn>
2024-08-16 17:45:43 +0800

    Changes
    Signed-off-by: guanyu.yi <guanyu.yi@invo.cn>

--- main.py
+++ main.py
@@ -23,3 +23,19 @@ def create_dataset():
                [2, 0, 0, 0, "no"]]
     labels = ['F1-AGE', 'F2-WORK', 'F3-HOME', 'F4-LOAN']
     return dataset, labels
+
+
+def create_tree(dataset, labels, feat_labels):
+    class_list = [example[-1] for example in dataset]
+    if class_list.count(class_list[0]) == len(class_list):
+        return class_list[0]
+    if len(dataset[0]) == 1:
+        return majority_count(class_list)
+    best_feat = choose_best_feature(dataset, labels)
+    best_feat_labels = feat_labels[best_feat]
+    mytree = {best_featlabel: {}}
+    del (labels[best_feat])
+    feat_values = [example[best_feat] for example in dataset]
+    uniquevals = set(feat_values)
+    for value in uniquevals:
+        mytree[best_featlabel][value] = create_tree(split_data(dataset, best_feat, value), labels, featlabels)
