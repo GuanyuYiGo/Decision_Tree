dac02b195855b758571a793a9fb73dfc946cf706
guanyu.yi <guanyu.yi@invo.cn>
2024-08-16 17:19:30 +0800

    Changes

---
+++ main.py
@@ -0,0 +1,25 @@
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
+from math import log
+import operator
+import pickle
+
+
+def create_dataset():
+    dataset = [[0, 0, 0, 0, "no"],
+               [0, 0, 0, 1, "no"],
+               [0, 1, 0, 1, "yes"],
+               [0, 1, 1, 0, "yes"],
+               [0, 0, 0, 0, "no"],
+               [1, 0, 0, 0, "no"],
+               [1, 0, 0, 1, "no"],
+               [1, 1, 1, 1, "yes"],
+               [1, 0, 1, 2, "yes"],
+               [1, 0, 1, 2, "yes"],
+               [2, 0, 1, 2, "yes"],
+               [2, 0, 1, 1, "yes"],
+               [2, 1, 0, 1, "yes"],
+               [2, 1, 0, 2, "yes"],
+               [2, 0, 0, 0, "no"]]
+    labels = ['F1-AGE', 'F2-WORK', 'F3-HOME', 'F4-LOAN']
+    return dataset, labels
